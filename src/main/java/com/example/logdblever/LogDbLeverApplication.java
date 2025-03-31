216e4a60cdcf123437d2fb566235904d3019bc01
kien <kienpt.dev.01@gmail.com>
2025-03-31 15:39:43 +0700

    init

---
+++ src/main/java/com/example/logdblever/LogDbLeverApplication.java
@@ -0,0 +1,13 @@
+package com.example.logdblever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogDbLeverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogDbLeverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/logdblever/LogDbLeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logdblever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogDbLeverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
